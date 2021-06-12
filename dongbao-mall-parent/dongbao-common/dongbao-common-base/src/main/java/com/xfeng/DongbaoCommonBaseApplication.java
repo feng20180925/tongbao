ff2e81e271fe8726b2dfd669dea032a0ed02a3cf
xfeng <2604858767@qq.com>
2021-06-12 20:16:59 +0800

    提交项目文件

---
+++ dongbao-mall-parent/dongbao-common/dongbao-common-base/src/main/java/com/xfeng/DongbaoCommonBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongbaoCommonBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongbaoCommonBaseApplication.class, args);
+    }
+
+}
